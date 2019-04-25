33c3d85b24a2ad30e56ebfa6c794348b8f6a2ee1
Katrin Haslinger <Katrin.Haslinger@gmx.at>
2019-04-25 14:06:22 +0200

    add basic project and components

---
+++ modelBrowser/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ModelsComponent } from './models/models.component';
+import { FilterComponent } from './filter/filter.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    ModelsComponent,
+    FilterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
